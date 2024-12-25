4ba6e487e7eb6a5ad712992839441bd4f9f4c5be
Toan Nguyen <vantoan1517@gmail.com>
2024-12-26 00:13:25 +0700

    Create header

--- components.d.ts
+++ components.d.ts
@@ -8,12 +8,17 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     Button: typeof import('primevue/button')['default']
+    Chip: typeof import('primevue/chip')['default']
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
     IconCommunity: typeof import('./src/components/icons/IconCommunity.vue')['default']
     IconDocumentation: typeof import('./src/components/icons/IconDocumentation.vue')['default']
     IconEcosystem: typeof import('./src/components/icons/IconEcosystem.vue')['default']
+    IconField: typeof import('primevue/iconfield')['default']
     IconSupport: typeof import('./src/components/icons/IconSupport.vue')['default']
     IconTooling: typeof import('./src/components/icons/IconTooling.vue')['default']
+    InputIcon: typeof import('primevue/inputicon')['default']
+    InputText: typeof import('primevue/inputtext')['default']
+    Message: typeof import('primevue/message')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
     TheWelcome: typeof import('./src/components/TheWelcome.vue')['default']
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,48 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const query = ref('')
+const hotTags = ref([
+  'laptop van phong',
+  'asus moi nhat',
+  'mac moi',
+  'laptop gaming',
+  'msi thin',
+  'ultra book',
+  'aspire 15',
+])
+</script>
 
 <template>
-  <header class="flex justify-center items-center">
-    <Button icon="pi pi-bars" label="Home"></Button>
+  <header class="bg-gradient-to-r from-primary-500 to-primary-300 py-5">
+    <div class="container mx-auto">
+      <div class="grid grid-cols-4">
+        <div class="flex">
+          <RouterLink to="/">
+            <img src="/src/assets/logo.svg" width="42" alt="logo" />
+          </RouterLink>
+        </div>
+        <div class="col-span-2 px-5">
+          <IconField>
+            <InputIcon class="pi pi-search" />
+            <InputText v-model="query" placeholder="Search" class="rounded-full" fluid />
+          </IconField>
+          <div class="flex justify-between gap-3 text-white overflow-hidden">
+            <RouterLink
+              v-for="tag in hotTags"
+              :key="tag"
+              to="/"
+              class="text-nowrap hover:underline underline-offset-4"
+            >
+              {{ tag }}
+            </RouterLink>
+          </div>
+        </div>
+        <div class="ml-auto space-x-5">
+          <Button icon="pi pi-user" severity="secondary" rounded />
+          <Button icon="pi pi-shopping-cart" label="Cart" severity="contrast" rounded />
+        </div>
+      </div>
+    </div>
   </header>
 </template>
